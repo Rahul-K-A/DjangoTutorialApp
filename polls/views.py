c4a6354e2596fe1525a50c1e1b9585db73b03dd5
Rahul-K-A <karahul209@gmail.com>
2022-07-05 12:33:16 +0530

    Tut 4: No generic views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Question
-from django.http import HttpResponse
+from .models import Question,Choice
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 
@@ -24,8 +25,28 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        '''request.POST is a dictionary-like object that lets you access submitted data by key name. In this case, request.POST['choice'] returns the ID of the selected choice, 
+        as a string. request.POST values are always strings.
+        "choice" comes from the name of the input tag in the detail.html form
+        '''
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        #reverse looks up the url for the name as defined in urls.py
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
